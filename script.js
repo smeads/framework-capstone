578a2b13df2a1db1d1e535a948d570444be31ebb
Steven Meads <sbmeads@gmail.com>
2017-02-14 21:13:19 -0800

    remove node_module and package.json

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// import Unsplash from 'unsplash-js';
-//
-// const unsplash = new Unsplash({
-//   applicationId: "d7da22b7d66c385e2832256137d0f82a0d4b19428f1984c5daa0160f47a27729",
-//   secret: "30b200cb9c201b3a1b3257ce9b2cf208577a209d913fe607e0dd97f7569c4cb7",
-//   callbackUrl: "urn:ietf:wg:oauth:2.0:oob"
-// });
-
 $(document).ready(function() {
   var todoItem = $("#todo-item");
   var url = 'https://api.unsplash.com/';
@@ -45,7 +37,6 @@ $(document).ready(function() {
     $("#header").text("TabIt!");
     $("#header").after("<p id='user-greet'>" + "Hello " + userStuff + ", " + "what would you like to tab?" + "</p>" );
   }
-
   // User input new todo
   $("#todo").submit(function(event) {
     event.preventDefault();
